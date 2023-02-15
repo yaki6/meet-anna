3c3dc9dcc66b37cafee775bd80ceabf7c9aee3ea
Yaqi Li <yaqi96@me.com>
2023-02-15 11:54:23 +0100

    adding openAI api

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,25 +3,22 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ["plugin:vue/essential", "@vue/standard"],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    semi: [0],
+    quotes: [0]
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)'
-      ],
+      files: ["**/__tests__/*.{j,t}s?(x)"],
       env: {
         jest: true
       }
     }
   ]
-}
+};
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template lang="pug">
 #app
-  img(
-    alt="Vue Bot UI",
-    src="./assets/logo.png"
-  )
+  img(alt="Vue Bot UI", src="./assets/logo.png")
   VueBotUI(
     :options="botOptions",
     :messages="messageData",
@@ -11,13 +8,20 @@
     :input-disable="inputDisable",
     :is-open="false",
     @init="botStart",
-    @msg-send="msgSend",
+    @msg-send="msgSend"
   )
 </template>
 <script>
-import BotIcon from './assets/icons/bot.png'
-import { VueBotUI } from './vue-bot-ui'
-import { messageService } from './helpers/message'
+import BotIcon from "./assets/icons/bot.png";
+import { VueBotUI } from "./vue-bot-ui";
+// import { messageService } from "./helpers/message";
+import axios from 'axios';
+const apiKey = "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";
+const client = axios.create({
+  headers: {
+    Authorization: "Bearer " + apiKey
+  }
+});
 
 export default {
   components: {
@@ -32,13 +36,13 @@ export default {
       inputDisable: false,
       botOptions: {
         botAvatarImg: BotIcon,
-        boardContentBg: '#f4f4f4',
-        msgBubbleBgBot: '#fff',
-        inputPlaceholder: 'Type hereeee...',
-        inputDisableBg: '#fff',
-        inputDisablePlaceholder: 'Hit the buttons above to respond'
+        boardContentBg: "#f4f4f4",
+        msgBubbleBgBot: "#fff",
+        inputPlaceholder: "Type hereeee..",
+        inputDisableBg: "#fff",
+        inputDisablePlaceholder: "Hit the buttons above to respond"
       }
-    }
+    };
   },
 
   methods: {
@@ -47,53 +51,63 @@ export default {
       // Request first message here
 
       // Fake typing for the first message
-      this.botTyping = true
+      this.botTyping = true;
       setTimeout(() => {
-        this.botTyping = false
+        this.botTyping = false;
         this.messageData.push({
-          agent: 'bot',
-          type: 'text',
-          text: 'Hello'
-        })
-      }, 1000)
+          agent: "bot",
+          type: "text",
+          text: "Hello, this is Anna. What do you want to talk about?"
+        });
+      }, 1000);
     },
 
     msgSend (value) {
       // Push the user's message to board
       this.messageData.push({
-        agent: 'user',
-        type: 'text',
+        agent: "user",
+        type: "text",
         text: value.text
-      })
+      });
 
-      this.getResponse()
+      this.getResponse(value.text);
     },
 
     // Submit the message from user to bot API, then get the response from Bot
-    getResponse () {
+    getResponse (text) {
       // Loading
-      this.botTyping = true
+      this.botTyping = true;
 
       // Post the message from user here
       // Then get the response as below
-
-      // Create new message from fake data
-      messageService.createMessage()
-        .then((response) => {
+      const params = {
+        prompt: text,
+        model: "text-davinci-003",
+        max_tokens: 100,
+        temperature: 0.7
+      };
+      // Create new message from openAI API
+      client
+        .post("https://api.openai.com/v1/completions", params)
+        .then((result) => {
+          const response = result.data.choices[0].text
           const replyMessage = {
-            agent: 'bot',
-            ...response
-          }
-
-          this.inputDisable = response.disableInput
-          this.messageData.push(replyMessage)
+            agent: "bot",
+            type: "text",
+            text: response
+          };
+          this.inputDisable = response.disableInput;
+          this.messageData.push(replyMessage);
 
           // finish
-          this.botTyping = false
+          this.botTyping = false;
         })
+        .catch((err) => {
+          console.log(err);
+        });
     }
   }
-}
+};
 </script>
 <style lang="scss">
 #app {
